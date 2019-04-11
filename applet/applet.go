d6911828d0ec44b047f52414ae2e6d42b8aa3953
bitebit <kfbygj@gmail.com>
2019-04-11 16:28:57 +0800

    feat: init

---
+++ applet/applet.go
@@ -0,0 +1,83 @@
+package applet
+
+import (
+	"context"
+
+	"github.com/BiteBit/applet"
+	"github.com/BiteBit/applet/api"
+)
+
+type (
+	// Applet 小程序
+	Applet struct {
+		name string
+
+		AppID       string `json:"appId"`
+		AppSecret   string `json:"appSecret"`
+		Token       string `json:"token"`
+		AesKey      string `json:"aesKey"`
+		APIDomain   string `json:"apiDomain"`
+		APIBasePath string `json:"apiBasePath"`
+		OriginID    string `json:"originId"`
+
+		*applet.Applet
+		tokenStore api.WechatTokenStore
+		before     api.Before
+		after      api.After
+	}
+)
+
+var (
+	// Default 默认的小程序sdk
+	Default = New("applet")
+)
+
+// Name 配置文件名称
+func (app *Applet) Name() string {
+	return app.name
+}
+
+// SetTokenStore 设置tokenstore
+func (app *Applet) SetTokenStore(tokenStore api.WechatTokenStore) {
+	app.tokenStore = tokenStore
+}
+
+// SetBefore set before hook
+func (app *Applet) SetBefore(before api.Before) {
+	app.before = before
+}
+
+// SetAfter set after hook
+func (app *Applet) SetAfter(after api.After) {
+	app.after = after
+}
+
+// ConfigWillLoad 配置文件将要加载
+func (app *Applet) ConfigWillLoad(context.Context) {
+
+}
+
+// ConfigDidLoad 配置文件已经加载。做一些默认值设置
+func (app *Applet) ConfigDidLoad(context.Context) {
+	mp := applet.NewApplet(
+		app.AppID,
+		app.AppSecret,
+		app.Token,
+		app.AesKey,
+		app.tokenStore,
+	)
+
+	mp.API.SetDomain(app.APIDomain)
+	mp.API.SetBasePath(app.APIBasePath)
+	mp.API.SetBefore(app.before)
+	mp.API.SetAftre(app.after)
+
+	app.Applet = mp
+}
+
+// New 新建一个加载指定配置文件的小程序sdk
+func New(name string) *Applet {
+	return &Applet{
+		name: name,
+	}
+}
---
+++ applet/mongo/mongo.go
@@ -0,0 +1,101 @@
+package mongo
+
+import (
+	"context"
+	"time"
+
+	"github.com/boxgo/kit/logger"
+	"github.com/boxgo/kit/mongo"
+	"github.com/globalsign/mgo/bson"
+)
+
+type (
+	// MgoTokenStore token存储器
+	MgoTokenStore struct {
+		name  string
+		mongo *mongo.Mongo
+
+		OriginID   string `json:"originId"`
+		DB         string `json:"db"`
+		Collection string `json:"collection"`
+	}
+
+	// wechatToken token db
+	wechatToken struct {
+		ExpireAt time.Time `bson:"expireAt"` // 参数发送时间
+		ExipreIn int       `bson:"exipreIn"` // 过期周期
+		Token    string    `bson:"token"`    // token值
+		OriginID string    `bson:"originId"` // 原始id
+	}
+)
+
+var (
+	// Default 默认的applet token store配置
+	Default = New("applet")
+)
+
+// Name token 配置文件名称
+func (ts *MgoTokenStore) Name() string {
+	return ts.name
+}
+
+// ConfigWillLoad 配置文件将要加载
+func (ts *MgoTokenStore) ConfigWillLoad(context.Context) {
+
+}
+
+// ConfigDidLoad 配置文件已经加载。做一些默认值设置
+func (ts *MgoTokenStore) ConfigDidLoad(context.Context) {
+	if ts.OriginID == "" {
+		panic("originId require config")
+	}
+
+	if ts.mongo == nil {
+		panic("mongo require config")
+	}
+
+	if ts.DB == "" {
+		ts.DB = "applet"
+	}
+
+	if ts.Collection == "" {
+		ts.Collection = "applet_token"
+	}
+}
+
+func (ts *MgoTokenStore) SetMongo(mongo *mongo.Mongo) {
+	ts.mongo = mongo
+}
+
+// Get 获取token
+func (ts *MgoTokenStore) Get() (token string, exipreIn int, expireAt time.Time) {
+	doc := wechatToken{}
+	err := ts.mongo.GetDB(ts.DB).C(ts.Collection).Find(bson.M{"originId": ts.OriginID}).One(&doc)
+	if err != nil {
+		logger.Default.Errorw("MgoTokenStore.Get.Error", "tokenStore", ts)
+	}
+
+	return doc.Token, doc.ExipreIn, doc.ExpireAt
+}
+
+// Set 保存token
+func (ts *MgoTokenStore) Set(token string, exipreIn int, expireAt time.Time) {
+	doc := wechatToken{
+		Token:    token,
+		ExipreIn: exipreIn,
+		ExpireAt: expireAt,
+		OriginID: ts.OriginID,
+	}
+	_, err := ts.mongo.GetDB(ts.DB).C(ts.Collection).Upsert(bson.M{"originId": ts.OriginID}, doc)
+
+	if err != nil {
+		logger.Default.Errorw("MgoTokenStore.Set.Error", "tokenStore", ts)
+	}
+}
+
+// New 新建一个指定名称的tokenstore
+func New(name string) *MgoTokenStore {
+	return &MgoTokenStore{
+		name: name,
+	}
+}
